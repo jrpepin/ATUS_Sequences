b1885bcd7b792fa0e45b403b4bb20b0ed3b85e1d
floo0017 <floo0017@umn.edu>
2019-06-19 16:07:00 -0500

    add file to install packages

---
+++ 00_packages.R
@@ -0,0 +1,55 @@
+#########This file is for installing required packages for sequence and cluster analysis, as well as further calculations. 
+
+if(!require(TraMineR)){
+  install.packages("TraMineR")
+  library(TraMineR)
+}
+
+if(!require(cluster)){
+  install.packages("cluster")
+  library(cluster)
+}
+
+if(!require(WeightedCluster)){
+  install.packages("WeightedCluster")
+  library(WeightedCluster)
+}
+
+if(!require(haven)){
+  install.packages("haven")
+  library(haven)
+}
+
+if(!require(foreign)){
+  install.packages("foreign")
+  library(foreign)
+}
+
+if(!require(dplyr)){
+  install.packages("dplyr")
+  library(dplyr)
+}
+
+if(!require(readr)){
+  install.packages("readr")
+  library(readr)
+}
+
+if(!require(here)){
+  install.packages("here")
+  library(here)
+}
+
+if(!require(ipumsr)){
+  install.packages("ipumsr")
+  library(ipumsr)
+}
+
+
+
+
+if(!require(tidyverse)){
+  install.packages("tidyverse")
+  library(tidyverse)
+}
+
